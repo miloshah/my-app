3928385992773a0a7f45a0220d8744b2dac25a62
miloni.sh <miloni.sh@media.net>
2022-01-12 16:03:24 +0530

    Change the recovered multiplier

--- src/util.js
+++ src/util.js
@@ -20,7 +20,7 @@ const casesTypeColors = {
     hex: "#7dd71d",
     rgb: "rgb(125, 215, 29)",
     half_op: "rgba(125, 215, 29, 0.5)",
-    multiplier: 1200,
+    multiplier: 300,
   },
   deaths: {
     hex: "#fb4443",
